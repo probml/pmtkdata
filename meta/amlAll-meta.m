b71b1c959e761d03966a9f117163e10ad4b33fc2
mattbdunham <mattbdunham@49fcfb0e-06c3-11df-adb7-1be7f7e3f636>
2010-08-23 23:52:09 +0000

    git-svn-id: https://pmtkdata.googlecode.com/svn/trunk@130 49fcfb0e-06c3-11df-adb7-1be7f7e3f636

---
+++ meta/amlAll-meta.m
@@ -0,0 +1,8 @@
+% PMTKdescription Molecular Classification of Cancer: Class Discovery and Class Prediction by Gene Expression
+% PMTKsource http://www.broadinstitute.org/cgi-bin/cancer/datasets.cgi
+% PMTKtypeX discrete
+% PMTKtypeY binary
+% PMTKncases 72
+% PMTKndims 7129
+% PMTKcreated processAmlAllData.m
+
