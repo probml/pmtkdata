f1af26f940556e5fef114fe6aeb2d44a78fa37bf
murphyk2 <murphyk2@49fcfb0e-06c3-11df-adb7-1be7f7e3f636>
2010-09-01 00:48:20 +0000

    git-svn-id: https://pmtkdata.googlecode.com/svn/trunk@136 49fcfb0e-06c3-11df-adb7-1be7f7e3f636

--- prostate/prostateDataMake.m
+++ prostate/prostateDataMake.m
@@ -8,9 +8,15 @@ names = {'lcavol', 'lweight', 'age',  'lbph', 'svi', 'lcp', 'gleason', 'pgg45', 
 istrain = [C{end}{:}]=='T'
 X = [C{2} C{3} C{4} C{5} C{6} C{7} C{8} C{9}];
 y = double([C{10}]);
-X = standardize(X);
 Xtrain = X(find(istrain),:);
 ytrain = y(find(istrain),:);
 Xtest = X(find(~istrain),:);
 ytest = y(find(~istrain),:);
 save('prostate.mat', 'Xtrain', 'ytrain', 'Xtest', 'ytest', 'X','y', 'names','istrain');
+
+X = standardize(X); % standardize outside of train/test split 
+Xtrain = X(find(istrain),:);
+ytrain = y(find(istrain),:);
+Xtest = X(find(~istrain),:);
+ytest = y(find(~istrain),:);
+save('prostateStnd.mat', 'Xtrain', 'ytrain', 'Xtest', 'ytest', 'X','y', 'names','istrain');
